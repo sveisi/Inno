ced5603a94aa3133612668fde05fe748af074f8b
sveisi <SirousWeisy@gmail.com>
2026-04-19 20:01:42 +0330

    ریفکتور نمایش باکس خطا در صفحات

---
+++ Views/Shared/HtmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Inno.Views.Shared
+{
+    public static class HtmlHelper
+    {
+        public static IHtmlContent ErrorContainer(this IHtmlHelper html)
+        {
+            return new HtmlString(@"
+            <div id='errordiv'
+                 class='alert alert-danger py-1 fw-semibold fade show'
+                 style='display:none'
+                 role='alert'></div>
+        ");
+        }
+    }
+}
