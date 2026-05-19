657607f12951b533878dd9d6d01e00f7c331a64d
sveisi <SirousWeisy@gmail.com>
2026-05-19 17:05:36 +0330

    چند نقش با هم

--- Types/UserRoleName.cs
+++ Types/UserRoleName.cs
@@ -5,5 +5,8 @@
         public const string Admin = nameof(UserRoleType.Admin);
         public const string Storekeeper = nameof(UserRoleType.Storekeeper);
         public const string Customer = nameof(UserRoleType.Customer);
+
+        public const string Admin_Storekeeper = nameof(UserRoleType.Admin)+","+ nameof(UserRoleType.Storekeeper);
+        public const string Admin_Storekeeper_Customer = nameof(UserRoleType.Admin) + "," + nameof(UserRoleType.Storekeeper) + "," + nameof(UserRoleType.Customer);
     }
 }
